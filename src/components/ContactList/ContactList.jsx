62b8e69198f328e4423d9bfe6fb464cd9e3144bd
Veakor <135655247+Veakor@users.noreply.github.com>
2024-04-26 11:27:29 +0200

    add

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,37 +1,35 @@
 import { useSelector } from "react-redux";
-import { useMemo } from "react";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
+import Loader from "../Loader/Loader";
+import ErrorMessenger from "../ErrorMessenger/ErrorMessenger";
 import Contact from "../Contact/Contact";
 
 import clsx from "clsx";
 import css from "./ContactList.module.css";
 
 const ContactList = () => {
-  const selectContacts = useSelector((state) => state.contacts.items);
-  const selectNameFilter = useSelector((state) => state.filters.name);
+  const loading = useSelector((state) => state.contacts.loading);
+  const errorMessenger = useSelector((state) => state.contacts.error);
 
-  const filteredContacts = useMemo(
-    () =>
-      selectContacts.filter((contact) => {
-        return contact.name
-          .toLowerCase()
-          .includes(selectNameFilter.toLowerCase());
-      }),
-    [selectNameFilter, selectContacts]
-  );
+  const filteredContacts = useSelector(selectFilteredContacts);
+
 
   return (
-    <ul className={clsx(css.contactsList)}>
-      {Array.isArray(filteredContacts) &&
-        filteredContacts.map((contact) => {
-          return <Contact key={contact.id} contact={contact} />;
-        })}
-    </ul>
+    <>
+    {loading && <Loader />}
+      {errorMessenger && <ErrorMessenger />}
+      <ul className={clsx(css.contactsList)}>
+        {Array.isArray(filteredContacts) &&
+          filteredContacts.map((contact) => {
+            return <Contact key={contact.id} contact={contact} />;
+          })}
+      </ul>
+    </>
   );
 };
 
 export default ContactList;
-
 
 
 
